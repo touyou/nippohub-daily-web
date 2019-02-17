5a107836c9a57ce76ffcca667f6873a473b0a6d2
宇山 拓夢 <sasurai.usagi3@gmail.com>
2019-02-17 09:27:58 +0900

    feature: indexざっくり作成

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <!-- TODO: コンポーネント分ける -->
+    <header>
+      <h1>NippoHub Dialy</h1>
+      <nav>
+        <ul>
+          <li>Sign in</li>
+          <li>Sign up</li>
+          <li>Sign out</li>
+        </ul>
+      </nav>
+    </header>
+    <!-- TODO: コンポーネント分ける -->
+    <ul>
+      <li v-for="dailyReport in dailyReports">
+        <a :href="'/dailyReposts/' + dailyReport.accessId">
+          <h2>{{ dailyReport.date }} {{ dailyReport.title }}</h2>
+          <span>{{ dailyReport.createdAt }}</span>
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script src="./javascripts/index.js"></script>
