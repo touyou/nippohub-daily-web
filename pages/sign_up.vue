e56c48bfd0efebcd92cd33bf13d21d42bf5565ce
宇山 拓夢 <sasurai.usagi3@gmail.com>
2019-02-20 07:39:07 +0900

    feature: sign upページ作成

---
+++ pages/sign_up.vue
@@ -0,0 +1,9 @@
+<template>
+  <form>
+    <input type="text" name="email" placeholder="email">
+    <input type="password" name="password" placeholder="password">
+    <input type="password" nane="password_confirmation" placeholder="password confirmation">
+    <button type="submit">サインアップ</button>
+  </form>
+</template>
+<script src="./javascripts/sign_up.js"></script>
