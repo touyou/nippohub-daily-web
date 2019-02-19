4e64f6572665a51a1ec0c9e8e5ae3c996605b535
宇山 拓夢 <sasurai.usagi3@gmail.com>
2019-02-20 07:54:40 +0900

    feature: firebaseのconfigを準備

---
+++ assets/javascripts/firebase_config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDYW753RcIC6_QmRTCJkK7DwuGIe0dhIBk',
+  authDomain: 'nippohub-daily-dev.firebaseapp.com',
+  databaseURL: 'https://nippohub-daily-dev.firebaseio.com',
+  projectId: 'nippohub-daily-dev',
+  storageBucket: '',
+  messagingSenderId: '754356144832'
+};
