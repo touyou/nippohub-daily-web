f63078f3c1c5cbe8f2491b27c2dca97b70918418
宇山 拓夢 <sasurai.usagi3@gmail.com>
2019-02-25 07:52:48 +0900

    feature: userIdの取得

--- pages/javascripts/index.js
+++ pages/javascripts/index.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+import FIREBASE_CONFIG from '~/assets/javascripts/firebase_config.js';
 import DailyReportForm from '~/components/DailyReportForm.vue';
 import DailyReportList from '~/components/DailyReportList.vue';
 
@@ -5,5 +7,17 @@ export default {
   components: {
     DailyReportForm,
     DailyReportList
+  },
+  data: function() {
+    return {uid: ''};
+  },
+  mounted: function() {
+    firebase.initializeApp(FIREBASE_CONFIG);
+
+    const auth = firebase.auth();
+
+    auth.onAuthStateChanged(user => {
+      this.currentUserId = user.uid;
+    });
   }
 };
