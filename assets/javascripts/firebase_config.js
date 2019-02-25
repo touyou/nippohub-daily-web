b9295e13d48ba92df40760fc12ee145ef3af945f
宇山 拓夢 <sasurai.usagi3@gmail.com>
2019-02-26 07:35:21 +0900

    fix: 使わなくなったから削除

--- assets/javascripts/firebase_config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  apiKey: 'AIzaSyDYW753RcIC6_QmRTCJkK7DwuGIe0dhIBk',
-  authDomain: 'nippohub-daily-dev.firebaseapp.com',
-  databaseURL: 'https://nippohub-daily-dev.firebaseio.com',
-  projectId: 'nippohub-daily-dev',
-  storageBucket: '',
-  messagingSenderId: '754356144832'
-};
