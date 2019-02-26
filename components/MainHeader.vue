3c350efb80135ad8cf4c43e5ea3e61af9f719a8a
宇山 拓夢 <sasurai.usagi3@gmail.com>
2019-02-27 07:32:22 +0900

    feature: ページのヘッダをコンポーネントに

---
+++ components/MainHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <h1>NippoHub Dialy</h1>
+    <nav>
+      <ul>
+        <li v-if="currentUserId !== null">Sign out</li>
+        <template v-else>
+          <li>Sign in</li>
+          <li>Sign up</li>
+        </template>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script src="./javascripts/main_header.js"></script>
