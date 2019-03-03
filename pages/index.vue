1b132ff06697e0048a469c06348f3cfd3dda83ce
宇山 拓夢 <sasurai.usagi3@gmail.com>
2019-03-04 07:36:05 +0900

    fix: サインインしていなければフォームなどを表示しない

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
-    <main-header :currentUserId="currentUserId"></main-header>
-    <daily-report-form :currentUserId="currentUserId"></daily-report-form>
-    <daily-report-list></daily-report-list>
+    <template v-if="currentUserId != null">
+      <main-header :currentUserId="currentUserId"></main-header>
+      <daily-report-form :currentUserId="currentUserId"></daily-report-form>
+      <daily-report-list></daily-report-list>
+    </template>
   </div>
 </template>
 
