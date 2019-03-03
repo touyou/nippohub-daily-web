500c621ffeae4c0c15ba9439623f01ff661b5339
宇山 拓夢 <sasurai.usagi3@gmail.com>
2019-03-03 09:32:54 +0900

    feature: ログインしていなければ日報作成しない

--- components/javascripts/daily_report_form.js
+++ components/javascripts/daily_report_form.js
@@ -9,6 +9,10 @@ export default {
     post: function() {
       const database = firebase.database();
 
+      if(this.currentUserId == null) {
+        return;
+      }
+
       database.ref('daily_reports/').push({
         content: this.content,
         createdAt: Date.now(), // TODO: タイムスタンプをサーバ側で生成する
