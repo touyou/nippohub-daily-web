2d7698e8578ccf7050356b74ca285301793dd8c6
宇山 拓夢 <sasurai.usagi3@gmail.com>
2019-03-07 07:45:33 +0900

    fix: 日報が見つからなかった時の処理を追加

--- components/javascripts/daily_report_detail.js
+++ components/javascripts/daily_report_detail.js
@@ -10,6 +10,10 @@ export default {
 
     database.ref(`/daily_reports/${this.dailyReportId}`).once('value', r => {
       const dailyReport = r.val();
+      if(dailyReport == null) {
+        return; // TODO: 日報が見つからなかった時の処理
+      }
+
       const createdAt = new Date(dailyReport.createdAt);
       // TODO: 日付などを0埋めして2桁に保てるようにする
       const createdAtStr = `${createdAt.getFullYear()}-${createdAt.getMonth()}-${createdAt.getDate()} ${createdAt.getHours()}:${createdAt.getMinutes()}`;
