36cc1b552823c036d3d023dc8cfbb4738866c139
宇山 拓夢 <sasurai.usagi3@gmail.com>
2019-03-08 07:35:28 +0900

    fix: currentUserIdの初期値が空文字だとif文が機能していないので、nullに変更

--- pages/javascripts/index.js
+++ pages/javascripts/index.js
@@ -10,7 +10,7 @@ export default {
     MainHeader
   },
   data: function() {
-    return {currentUserId: ''};
+    return {currentUserId: null};
   },
   mounted: function() {
     const auth = firebase.auth();
