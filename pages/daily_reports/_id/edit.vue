1b8f80f9278a348fa99be0a6db840f7f46d8fddf
宇山 拓夢 <sasurai.usagi3@gmail.com>
2019-03-10 07:48:30 +0900

    add: 削除ボタンを追加

--- pages/daily_reports/_id/edit.vue
+++ pages/daily_reports/_id/edit.vue
@@ -1,5 +1,8 @@
 <template>
-  <daily-report-form :currentUserId="currentUserId" :dailyReportId="$route.params.id" v-if="currentUserId != null"></daily-report-form>
+  <div>
+    <daily-report-form :currentUserId="currentUserId" :dailyReportId="$route.params.id" v-if="currentUserId != null"></daily-report-form>
+    <ButtonDeletingDailyReport :dailyReportId="$route.params.id"></ButtonDeletingDailyReport>
+  </div>
 </template>
 
 <script src="~/pages/javascripts/daily_report_edit.js"></script>
