e2fc9faf8dd928c9c71796e0c077867546a23396
宇山 拓夢 <sasurai.usagi3@gmail.com>
2019-03-15 07:51:17 +0900

    add: ヘッダデザイン(仮)

--- components/MainHeader.vue
+++ components/MainHeader.vue
@@ -1,16 +1,19 @@
 <template>
-  <header>
-    <h1>NippoHub Dialy</h1>
-    <nav>
-      <ul>
-        <li v-if="currentUserId !== null" @click="signOut">Sign out</li>
-        <template v-else>
-          <li><a href="/sign_in">Sign in</a></li>
-          <li><a href="/sign_up">Sign up</a></li>
-        </template>
-      </ul>
-    </nav>
+  <header class="p-header">
+    <div class="p-header__container">
+      <h1 class="p-page-title">NippoHub Dialy</h1>
+      <nav>
+        <ul class="p-auth-links">
+          <li class="p-auth-links__item" v-if="currentUserId !== null" @click="signOut">Sign out</li>
+          <template v-else>
+            <li class="p-auth-links__item"><a href="/sign_in">Sign in</a></li>
+            <li class="p-auth-links__item"><a href="/sign_up">Sign up</a></li>
+          </template>
+        </ul>
+      </nav>
+    </div>
   </header>
 </template>
 
 <script src="./javascripts/main_header.js"></script>
+<style src="~/assets/header.css"></style>
