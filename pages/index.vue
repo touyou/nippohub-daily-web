c0cca26f589d0aae1c46533d0a4297aff73c9a60
宇山 拓夢 <sasurai.usagi3@gmail.com>
2019-03-16 07:22:57 +0900

    add: トップページコンテナ実装

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
     <main-header :currentUserId="currentUserId"></main-header>
-    <template v-if="currentUserId != null">
+    <div class="l-container" v-if="currentUserId != null">
       <daily-report-form :currentUserId="currentUserId"></daily-report-form>
       <daily-report-list :currentUserId="currentUserId"></daily-report-list>
-    </template>
+    </div>
   </div>
 </template>
 
