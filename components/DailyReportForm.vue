d17fccc6b526599833957b2cb90efcf9380a3782
宇山 拓夢 <sasurai.usagi3@gmail.com>
2019-03-17 08:29:56 +0900

    add: 日報投稿フォームのデザイン

--- components/DailyReportForm.vue
+++ components/DailyReportForm.vue
@@ -1,10 +1,17 @@
 <template>
-  <form @submit.prevent="post">
-    <input type="date" placeholder="日付" v-model="date">
-    <input type="text" placeholder="title" v-model="title">
-    <textarea name="content" v-model="content"></textarea>
-    <button type="submit">送信</button>
+  <form class="p-daily-report-form" @submit.prevent="post">
+    <div>
+      <input class="p-oneline-input p-daily-report-form__date-form" type="date" placeholder="日付" v-model="date">
+      <input class="p-oneline-input p-daily-report-form__title-form" type="text" placeholder="title" v-model="title">
+    </div>
+    <div class="u-mt-075" style="line-height: 1">
+      <textarea class="p-daily-report-form__content-form" name="content" v-model="content"></textarea>
+    </div>
+    <div class="u-text-align-right u-mt-075">
+      <button class="p-daily-report-form__submit" type="submit">送信</button>
+    </div>
   </form>
 </template>
 
 <script src="./javascripts/daily_report_form.js"></script>
+<style src="~/assets/stylesheets/daily_report_form.css"></style>
