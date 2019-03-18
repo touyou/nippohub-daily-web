28a5a2130cf9075c0153f67d2279f68f8e449b01
宇山 拓夢 <sasurai.usagi3@gmail.com>
2019-03-19 07:56:14 +0900

    add: 認証フォームデザイン

--- pages/sign_up.vue
+++ pages/sign_up.vue
@@ -1,9 +1,27 @@
 <template>
-  <form @submit.prevent="signUp">
-    <input type="text" name="email" placeholder="email" v-model="email">
-    <input type="password" name="password" placeholder="password" v-model="password">
-    <input type="password" nane="password_confirmation" placeholder="password confirmation" v-model="passwordConfirmation">
-    <button type="submit">サインアップ</button>
-  </form>
+  <div>
+    <main-header :currentUserId="null"></main-header>
+    <div class="l-container p-auth-form">
+      <form @submit.prevent="signUp">
+        <div>
+          <input class="p-oneline-input p-auth-form__input" type="text" name="email" placeholder="email" v-model="email">
+        </div>
+        <div class="u-mt-075">
+          <input class="p-oneline-input p-auth-form__input" type="password" name="password" placeholder="password" v-model="password">
+        </div>
+        <div class="u-mt-075">
+          <input class="p-oneline-input p-auth-form__input" type="password" nane="password_confirmation" placeholder="password confirmation" v-model="passwordConfirmation">
+        </div>
+        <div class="u-mt-075 u-text-align-right">
+          <button class="p-submit" type="submit">サインアップ</button>
+        </div>
+      </form>
+    </div>
+  </div>
 </template>
+
 <script src="./javascripts/sign_up.js"></script>
+<style src="~/assets/stylesheets/submit.css"></style>
+<style src="~/assets/stylesheets/oneline_input.css"></style>
+<style src="~/assets/stylesheets/auth_form.css"></style>
+<style src="~/assets/stylesheets/utility.css"></style>
