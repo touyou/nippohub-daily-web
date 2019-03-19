7bd7ac64e67acd335f0c9bba7a71fca6157557b3
宇山 拓夢 <sasurai.usagi3@gmail.com>
2019-03-20 07:52:11 +0900

    add: メタタグ設定

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,9 +4,18 @@ export default {
     dir: 'docs'
   },
   head: {
-    title: 'NippoHub Daily',
+    title: 'NippoHub',
     meta: [
-      {charset: 'utf-8'}
+      {charset: 'utf-8'},
+      {description: '日報管理サービス'},
+      {property: 'og:type', content: 'website'},
+      {property: 'og:title', content: 'NippoHub'},
+      {property: 'og:description', content: '日報管理サービス'},
+      {property: 'og:site_name', content: 'NippoHub'},
+      {name: 'twitter:card', content: 'summary'},
+      {name: 'twitter:creator', content: '@sasurai_usagi3'},
+      {name: 'twitter:title', content: 'NippoHub'},
+      {name: 'twitter:description', content: '日報管理サービス'}
     ]
   },
   css: [
