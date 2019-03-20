8fb5eba13cd8295d72d6527a82b8b6cceb35f5bd
宇山 拓夢 <sasurai.usagi3@gmail.com>
2019-03-21 08:31:58 +0900

    update: デザイン漏れてた箇所のデザイン実装

--- pages/daily_reports/_id/index.vue
+++ pages/daily_reports/_id/index.vue
@@ -1,5 +1,10 @@
 <template>
-  <DailyReportDetail :daily-report-id="$route.params.id"></DailyReportDetail>
+  <div>
+    <main-header :currentUserId="currentUserId"></main-header>
+    <div class="l-container">
+      <DailyReportDetail :daily-report-id="$route.params.id"></DailyReportDetail>
+    </div>
+  </div>
 </template>
 
 <script src="~/pages/javascripts/daily_report.js">
