878d6c8f33f251016363b8fa7a6559ebc24cc12f
宇山 拓夢 <sasurai.usagi3@gmail.com>
2019-04-01 20:16:39 +0900

    add: インポート機能実装

---
+++ pages/import.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <main-header :currentUserId="currentUser"></main-header>
+    <div class="l-container">
+      <form class="p-import-form">
+        <textarea class="p-import-form__text-area" name="daily_reports" v-model="dailyReportsJson"></textarea>
+        <div class="u-mt-075 u-text-align-right">
+          <button class="p-submit" @click.prevent="requestToImport">インポート</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script src="./javascripts/import.js"></script>
+<style src="~/assets/stylesheets/submit.css"></style>
+<style src="~/assets/stylesheets/import_form.css"></style>
+<style src="~/assets/stylesheets/utility.css"></style>
