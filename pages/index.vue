e163c725b1710e93f5a72dc5bde21f7c1afb8979
宇山 拓夢 <sasurai.usagi3@gmail.com>
2019-04-05 07:47:04 +0900

    add: 全削除機能追加

--- pages/index.vue
+++ pages/index.vue
@@ -7,9 +7,13 @@
         <h2 class="p-sub-title">過去の日報(最大30件)</h2>
         <daily-report-list :currentUserId="currentUserId"></daily-report-list>
       </div>
+      <div class="u-text-align-right u-mt-075">
+        <button type="button" class="p-btn" @click="deleteAll">全削除する</button>
+      </div>
     </div>
   </div>
 </template>
 
 <script src="~/assets/javascripts/pages/index.js"></script>
 <style src="~/assets/stylesheets/sub_title.css"></style>
+<style src="~/assets/stylesheets/utility.css"></style>
