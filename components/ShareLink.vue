c5ec6fc1008daaa7f64021bfba0b63013707144a
宇山 拓夢 <sasurai.usagi3@gmail.com>
2019-04-07 11:11:44 +0900

    update: access keyの生成方法

---
+++ components/ShareLink.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <template v-if="accessKey != null">
+      <div>共有URL: <input type="text" readonly :value="`https://nippohub.com/daily_reports/public/${accessKey}`" style="width: 100%"></div>
+      <button type="button" class="p-submit" @click="dismissAccessKey">プライベートにする</button>
+    </template>
+    <button type="button" class="p-submit" v-else @click="generateAccessKey">共有する</button>
+  </div>
+</template>
+
+<script src="~/assets/javascripts/components/share_link.js"></script>
+<style src="~/assets/stylesheets/submit.css"></style>
