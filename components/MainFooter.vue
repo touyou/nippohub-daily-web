9aa44af1338ad62033948299b005a8489635fabf
宇山 拓夢 <sasurai.usagi3@gmail.com>
2019-04-20 10:43:53 +0900

    add: Footerのコンポーネント追加

---
+++ components/MainFooter.vue
@@ -0,0 +1,13 @@
+<template>
+  <footer class="p-footer">
+    <div>
+      <span>
+        &copy;NippoHub
+      </span>
+      <nuxt-link to="/agreements" target="_blank">利用規約</nuxt-link>
+      <nuxt-link to="/privacy" target="_blank">プライバシーポリシー</nuxt-link>
+    </div>
+  </footer>
+</template>
+
+<style src="~/assets/stylesheets/footer.css"></style>
